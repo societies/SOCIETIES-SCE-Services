af2501a589bb6c3d0ec3c7f2e861fc83d2f38f85
Guido Spadotto <gspadotto@soluta.net>
2012-06-08 09:12:21 +0200

    Fixed enumeration values

--- 3rdPartyServices/EnterpriseServices/Calendar/org.societies.sharedCalendar/src/main/java/org/societies/rdPartyService/enterprise/sharedCalendar/commsServer/SharedCalendarCommServer.java
+++ 3rdPartyServices/EnterpriseServices/Calendar/org.societies.sharedCalendar/src/main/java/org/societies/rdPartyService/enterprise/sharedCalendar/commsServer/SharedCalendarCommServer.java
@@ -128,7 +128,7 @@ public class SharedCalendarCommServer implements IFeatureServer{
 				List<Calendar> retrievedCalendars = this.sharedCalendarService.retrieveCISCalendarList(bean.getCISId());
 				resultBean.setCalendarList(retrievedCalendars);
 				break;
-			case RETRIEVE_CALENDAR_EVENTS:
+			case RETRIEVE_CIS_CALENDAR_EVENTS:
 				List<Event> retrievedEvents = this.sharedCalendarService.retrieveCalendarEvents(bean.getCalendarId());
 				resultBean.setEventList(retrievedEvents);
 				break;
@@ -157,7 +157,7 @@ public class SharedCalendarCommServer implements IFeatureServer{
 					resultBean.setEventId(returnedEventId);
 				}
 				break;
-			case RETRIEVE_EVENTS_PRIVATE_CALENDAR:
+			case RETRIEVE_EVENTS_ON_PRIVATE_CALENDAR:
 				String calendarIdForAllEvents = this.sharedCalendarService.retrievePrivateCalendarId(stanza.getFrom().getJid());
 				if (calendarIdForAllEvents != null) {
 					List<Event> returnedPrivateCalendarEventList = this.sharedCalendarService.retrieveCalendarEvents(calendarIdForAllEvents);
