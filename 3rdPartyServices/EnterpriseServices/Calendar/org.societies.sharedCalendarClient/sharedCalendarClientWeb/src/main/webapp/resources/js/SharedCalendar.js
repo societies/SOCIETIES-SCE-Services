fdd2198c4d3ad77b17a8319191a10f647082b4ef
apanazzolo-sn <apanazzolo@soluta.net>
2012-07-25 12:41:24 +0200

    added mobile detect for calendar web app started integration with CIS feed.

--- 3rdPartyServices/EnterpriseServices/Calendar/org.societies.sharedCalendarClient/sharedCalendarClientWeb/src/main/webapp/resources/js/SharedCalendar.js
+++ 3rdPartyServices/EnterpriseServices/Calendar/org.societies.sharedCalendarClient/sharedCalendarClientWeb/src/main/webapp/resources/js/SharedCalendar.js
@@ -13,11 +13,52 @@ var eventData = {
 //JQuery Stuff
 jQuery.noConflict();	
 jQuery(document).ready(function($) {
+	var ua = navigator.userAgent.toLowerCase();
+	var isAndroid = ua.indexOf("android") > -1; //&& ua.indexOf("mobile");
+	if(isAndroid) {
+		$('#calendar').weekCalendar({
+			timeslotsPerHour: 2,
+			height: function($calendar){
+				return $(window).height() - $('#divHeader').outerHeight();
+			},
+			eventRender : function(calEvent, $event) {
+				if(calEvent.end.getTime() < new Date().getTime()) {
+					$event.css("backgroundColor", "#aaa");
+					$event.find(".time").css({"backgroundColor": "#999", "border":"1px solid #888"});
+				}
+			},
+			eventNew : function(calEvent, $event) {
+				displayMessage("<strong>Added event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+				alert("You've added a new event. You would capture this event, add the logic for creating a new event with your own fields, data and whatever backend persistence you require.");
+			},
+			eventDrop : function(calEvent, $event) {
+				displayMessage("<strong>Moved Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+			},
+			eventResize : function(calEvent, $event) {
+				displayMessage("<strong>Resized Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+			},
+			eventClick : function(calEvent, $event) {
+				displayMessage("<strong>Clicked Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+			},
+			eventMouseover : function(calEvent, $event) {
+				displayMessage("<strong>Mouseover Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+			},
+			eventMouseout : function(calEvent, $event) {
+				displayMessage("<strong>Mouseout Event</strong><br/>Start: " + calEvent.start + "<br/>End: " + calEvent.end);
+			},
+			noEvents : function() {
+				displayMessage("There are no events for this week");
+			},
+			data: eventData
+		
+		});
+	}else{
+
 
 	$('#calendar').weekCalendar({
 		timeslotsPerHour: 4,
 		height: function($calendar){
-			return $(window).height() - $("h1").outerHeight();
+			return $(window).height() - $('#divHeader').outerHeight();
 		},
 		eventRender : function(calEvent, $event) {
 			if(calEvent.end.getTime() < new Date().getTime()) {
@@ -48,7 +89,9 @@ jQuery(document).ready(function($) {
 			displayMessage("There are no events for this week");
 		},
 		data: eventData
+	
 	});
+	}
 
 	function displayMessage(message) {
 		$("#message").html(message).fadeIn();
@@ -88,8 +131,9 @@ $("getCisCalendarEventsButton").on("click", function(event){
 			      var ajResp = eval("(" + response + ")"); 
 			      //alert("Success! \n\n" + response);
 			      eventData.events = ajResp;
-			      jQuery("#calendar").weekCalendar('refresh'); 
 			      jQuery("#calendar").show();
+			      jQuery("#calendar").weekCalendar('refresh'); 
+			      
 			    },
 			    onFailure: function(){ alert('Something went wrong...'); }
 			  });			
@@ -142,8 +186,9 @@ new Ajax.Updater('result','/sharedCal/getPrivateEvents.do',
 		      var ajResp = eval("(" + response + ")"); 
 		      //alert("Success! \n\n" + response);
 		      eventData.events = ajResp;
-		      jQuery("#calendar").weekCalendar('refresh'); 
 		      jQuery("#calendar").show();
+		      jQuery("#calendar").weekCalendar('refresh'); 
+		     
 		    },
 		    onFailure: function(){ alert('Something went wrong...'); }
 		  });			
