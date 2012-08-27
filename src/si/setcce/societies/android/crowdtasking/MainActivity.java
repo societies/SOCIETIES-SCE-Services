f8956090d8ce3a2caaa9cfaa5d85c2c57c39614b
Simon <sjuresa@gmail.com>
2012-08-27 15:18:37 +0200

    initial commit

---
+++ src/si/setcce/societies/android/crowdtasking/MainActivity.java
@@ -0,0 +1,118 @@
+package si.setcce.societies.android.crowdtasking;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
+public class MainActivity extends Activity {
+	private WebView webView;
+	
+	@SuppressLint("SetJavaScriptEnabled")
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+		
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_main);
+		webView = (WebView) findViewById(R.id.webView1);
+		webView.getSettings().setJavaScriptEnabled(true);
+        webView.setInitialScale(1);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+        webView.getSettings().setUseWideViewPort(true);
+        webView.getSettings().setBuiltInZoomControls(true);
+        webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
+        webView.setScrollbarFadingEnabled(false);
+        webView.setWebViewClient(new MyWebViewClient());
+        webView.getSettings().setDefaultZoom(WebSettings.ZoomDensity.FAR);
+        webView.loadUrl("http://crowdtasking.appspot.com/mobile.html");
+     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed()
+    {
+        if(webView.canGoBack())
+            webView.goBack();
+        else
+            super.onBackPressed();
+    }    
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.scanQRcode:
+                //scan;
+                IntentIntegrator integrator = new IntentIntegrator(MainActivity.this);
+                integrator.initiateScan(IntentIntegrator.QR_CODE_TYPES);
+
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+      IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+      if (result != null) {
+        String contents = result.getContents();
+        if (contents != null) {
+        	//Toast toast = Toast.makeText(getApplicationContext(), R.string.result_succeeded, Toast.LENGTH_SHORT);
+        	//toast.show();
+        	String resultQR = result.getContents();
+        	if (resultQR.startsWith("http")) {
+            	webView.loadUrl(resultQR);
+        	}
+        	if (resultQR.startsWith("cs:")) {
+        		webView.loadUrl(resultQR.replaceFirst("cs", "http"));
+        	}
+        } else {
+        	Toast toast = Toast.makeText(getApplicationContext(), R.string.result_failed, Toast.LENGTH_SHORT);
+        	toast.show();
+        	toast = Toast.makeText(getApplicationContext(), getString(R.string.result_failed_why), Toast.LENGTH_LONG);
+        	toast.show();
+        }
+      }
+    }
+    
+    private class MyWebViewClient extends WebViewClient {
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+        
+        public void onPageFinished(WebView view, String url) {
+        	if (url.contains("enter") || url.contains("leave")) {
+        		webView.goBack();
+        	}
+            view.setInitialScale((int)(25*view.getScale()));
+        }
+    }
+}
