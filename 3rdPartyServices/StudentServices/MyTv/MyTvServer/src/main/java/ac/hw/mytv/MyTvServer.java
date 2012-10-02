e982f2d8133d6f0573c3d0105f15d6994acc3444
Sarah Gallacher <s.gallacher@hw.ac.uk>
2012-10-02 14:43:56 +0100

    Updates and missing files added

--- 3rdPartyServices/StudentServices/MyTv/MyTvServer/src/main/java/ac/hw/mytv/MyTvServer.java
+++ 3rdPartyServices/StudentServices/MyTv/MyTvServer/src/main/java/ac/hw/mytv/MyTvServer.java
@@ -4,8 +4,16 @@ package ac.hw.mytv;
  * Hello world!
  *
  */
-public class App 
+public class MyTvServer
 {
+	
+	public MyTvServer(){	
+	}
+	
+	public void initialiseMyTvServer(){
+		
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
