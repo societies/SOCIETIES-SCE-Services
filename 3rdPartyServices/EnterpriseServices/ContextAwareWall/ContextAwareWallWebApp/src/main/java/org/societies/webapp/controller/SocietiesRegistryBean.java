a8f7abd53f34f9fd13e2fe67d706660c286dfdfa
guyf <guyf@GUYF-TP.haifa.ibm.com>
2012-10-03 14:32:09 +0100

    updates to the 3rp web-app

--- 3rdPartyServices/EnterpriseServices/ContextAwareWall/ContextAwareWallWebApp/src/main/java/org/societies/webapp/controller/SocietiesRegistryBean.java
+++ 3rdPartyServices/EnterpriseServices/ContextAwareWall/ContextAwareWallWebApp/src/main/java/org/societies/webapp/controller/SocietiesRegistryBean.java
@@ -8,7 +8,7 @@ public class SocietiesRegistryBean implements ISocietiesRegistryBean {
 	private String serverURL; 
 	private String mockEntity;
 	private boolean mockEntityActive;
-	
+	private String pzQueriesURL;
 	
 	public String getServerURL() {
 		return serverURL;
@@ -28,4 +28,11 @@ public class SocietiesRegistryBean implements ISocietiesRegistryBean {
 	public void setMockEntityActive(boolean mockEntityActive) {
 		this.mockEntityActive = mockEntityActive;
 	}
+	public String getPzQueriesURL() {
+		return pzQueriesURL;
+	}
+	public void setPzQueriesURL(String pzQueriesURL) {
+		this.pzQueriesURL = pzQueriesURL;
+	}
+	
 }
