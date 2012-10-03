abf85f55d7dbfd5231c94a83d827343473c43642
XIONG Haoyi <xhyccc@gmail.com>
2012-10-03 20:43:03 +0200

    phone server com

--- 3rdPartyServices/StudentServices/NearMe/Android/src/com/asocom/tools/Server.java
+++ 3rdPartyServices/StudentServices/NearMe/Android/src/com/asocom/tools/Server.java
@@ -35,7 +35,7 @@ import android.util.Log;
 public class Server {
 
 	/** The address. */
-	public static String address = "http://137.195.108.222:8080/nearme/";
+	public static String address = "http://societies.local.macs.hw.ac.uk:18080/nearme/";
 	// public static String address = "http://192.168.191.1/asocom.php";
 	// public static String address = "http://todotest.netau.net/index2.php";
 	
