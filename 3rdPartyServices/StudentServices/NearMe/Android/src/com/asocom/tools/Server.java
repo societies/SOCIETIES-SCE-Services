57c732feeaee40b2c29e5847d5cdf254ffa1cf53
XIONG Haoyi <xhyccc@gmail.com>
2012-10-04 12:14:54 +0200

    IPAddress change

--- 3rdPartyServices/StudentServices/NearMe/Android/src/com/asocom/tools/Server.java
+++ 3rdPartyServices/StudentServices/NearMe/Android/src/com/asocom/tools/Server.java
@@ -35,7 +35,8 @@ import android.util.Log;
 public class Server {
 
 	/** The address. */
-	public static String address = "http://137.195.108.202:8080/nearme/";
+	public static String address = "http://societies.local.macs.hw.ac.uk:18080/nearme/";
+	//"http://137.195.108.202:8080/nearme/";
 	//"http://societies.local.macs.hw.ac.uk:18080/nearme/";
 	// public static String address = "http://192.168.191.1/asocom.php";
 	// public static String address = "http://todotest.netau.net/index2.php";
