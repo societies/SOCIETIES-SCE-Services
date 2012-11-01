6d09e7252ad67d31584c9c84aec89e217594a52d
tcarlyle <tcarlyle@gmail.com>
2012-11-01 15:12:29 +0100

    got the message forwarding working

--- 3rdPartyServices/DisasterManagement/IJacket/ijacket/src/main/java/org/societies/thirdpartyservices/ijacket/IJacketScanActiv.java
+++ 3rdPartyServices/DisasterManagement/IJacket/ijacket/src/main/java/org/societies/thirdpartyservices/ijacket/IJacketScanActiv.java
@@ -24,6 +24,8 @@ import android.content.Context;
 import android.content.CursorLoader;
 import android.content.Intent;
 import android.content.Loader;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -54,9 +56,10 @@ public class IJacketScanActiv extends Activity{// implements OnItemSelectedListe
     //private BluetoothConnection con;
     private static final int IJACK_SCAN_ACTV_CONTENT_VIEW_ID = 10101010;
     private TableLayout layout;
-    private Button disconnectButton;
     private Button scanButton;
-    ContentResolver cr;
+    
+    
+    private Button preferredJackButton;
     //Loader l;
     //private Spinner spinnerCIS;
 	
@@ -69,14 +72,8 @@ public class IJacketScanActiv extends Activity{// implements OnItemSelectedListe
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d(LOG_TAG, "IJacketScanActiv onCreate");
-        cr = this.getApplication().getContentResolver();
         
 
-        
-		IJacketApp appState = ((IJacketApp)getApplicationContext());
-		BluetoothConnection con = appState.getCon();
-		if(con == null || con.isConnected() == false){
-			Log.d(LOG_TAG, "connection does not exist on IJacketScanActiv onCreate");  
 	        layout = new TableLayout(this);
 	        layout.setId(IJACK_SCAN_ACTV_CONTENT_VIEW_ID);
 	        layout.setLayoutParams( new TableLayout.LayoutParams(TableLayout.LayoutParams.MATCH_PARENT, TableLayout.LayoutParams.MATCH_PARENT) );
@@ -105,25 +102,9 @@ public class IJacketScanActiv extends Activity{// implements OnItemSelectedListe
 	        resetUI();
 	        
 	        
-	        //Initialize disconnect/reconnect button
-	        disconnectButton = new Button(this);
-	        disconnectButton.setText("Disconnect");
-	        disconnectButton.setOnClickListener( new OnClickListener(){
-	
-				public void onClick(View v) {
-					disconnect();
-					resetUI();
-				}
-	        	
-	        });
 	        
 	        super.setContentView(layout);
-		}
-		else{// connection already exist
-			Log.d(LOG_TAG, "connection already exist on IJacketScanActiv onCreate");
-			Intent intent = new Intent(IJacketScanActiv.this, JacketMenuActivity.class);
-			startActivity(intent);
-		}
+
     }
     
 
@@ -211,112 +192,28 @@ public class IJacketScanActiv extends Activity{// implements OnItemSelectedListe
             	 //ImageView image = new ImageView(IJacketScanActiv.this);
             	 //image.setImageResource(R.drawable.scan);
             	 //layout.addView(image);
+            	 
+            	 SharedPreferences mypref = getSharedPreferences(IJacketApp.PREF_FILE_NAME, MODE_PRIVATE);
+            	 if(mypref.contains(IJacketApp.MAC_PREFERENCE_TAG)){
+            		 Log.d(LOG_TAG, "adding last jacket button");
+            		 preferredJackButton = new Button(IJacketScanActiv.this);
+				 	 preferredJackButton.setText("Connect to last jacket");
+		 		 	 preferredJackButton.setOnClickListener( new OnClickListener(){
+
+		 				public void onClick(View v) {
+		 					Intent intent = new Intent(IJacketScanActiv.this, JacketMenuActivity.class);
+		 					startActivity(intent);};
+		 		 	 });
+		 		 	layout.addView(preferredJackButton);
+		 		 }
+
              }
         });
     }
         
-    private void disconnect(){
-		IJacketApp appState = ((IJacketApp)getApplicationContext());
-		BluetoothConnection con = appState.getCon();
-		if(con != null) con.disconnect();
-    }
 
-    
-    private ConnectionListener getConnectionListener(Activity parentActivity) {
-        return new myConList(parentActivity);
-    }
-    
-    public class myConList implements ConnectionListener {
 
-    	
-    	private Activity parentActivity;
-    	
-    	public myConList(Activity parentActivity){
-    		super();
-    		this.parentActivity = parentActivity;
-    		Log.d(LOG_TAG, "conlist created");
-    	}
-    	
-        public void onConnect(BluetoothConnection bluetoothConnection) {
-    		IJacketApp appState = ((IJacketApp)getApplicationContext());
-    		appState.registerConnect();
-        	Log.d(LOG_TAG, "Blueetooth onconnect");
-            quickToastMessage("Connected! (" + bluetoothConnection.toString() + ")");
-            clearUI();
-            //addButton(disconnectButton);
-            
-            Intent intent = new Intent(IJacketScanActiv.this, JacketMenuActivity.class);
-            
-            //Add a button for every service found
-            ConnectionMetadata meta = bluetoothConnection.getConnectionData();
-			for(String service : meta.getServicesSupported()) {
-				Integer pins[] = meta.getServicePins(service);
-				
-				//Pin controlled button
-				if(pins.length > 0) {
-					// ILL just add one button of each
-					if(service.equals(DefaultServices.SERVICE_LED_LAMP.name())) intent.putExtra("LED PIN", pins[0]);  
-					if(service.equals(DefaultServices.SERVICE_VIBRATION.name())) intent.putExtra("VIBRATION PIN", pins[0]);
-					if(service.equals(DefaultServices.SERVICE_SPEAKER.name())) intent.putExtra("SPEAKER PIN", pins[0]);
-				}
-			}
 
-            startActivity(intent);
-
-			
-            
-        }
-
-        public void onConnecting(BluetoothConnection bluetoothConnection) {
-            quickToastMessage("Connecting");
-        }
-
-        public void onDisconnect(BluetoothConnection bluetoothConnection) {
-    		IJacketApp appState = ((IJacketApp)getApplicationContext());
-
-        	if(false == appState.isConectStatus() && false == appState.testMaxRetryCounter()){
-        		
-				quickToastMessage("Disconnected, going to retry with " + appState.getMacAddress());
-				Log.d(LOG_TAG, "Disconnected, going to retry with " + appState.getMacAddress() + ", iteration " + appState.getRetrycounter());
-				BluetoothConnection con = appState.getCon();
-				
-				if (null != con) 
-					if(con.isConnected()) // double check if it is really disconnected
-						appState.registerConnect();
-						
-				//while(false == appState.isConectStatus() && false == appState.testMaxRetryCounter()){
-	            	try {
-						con = new BluetoothConnection(appState.getMacAddress(), parentActivity, this);
-						con.connect();
-						appState.setCon(con);
-						//appState.registerConnect();
-					} catch (IllegalArgumentException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (ComLibException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-	            	appState.incRetryCounter();
-	            	Log.d(LOG_TAG, "retry counter iterated");
-				//}
-
-        	}else{
-                quickToastMessage("Disconnected for good");
-                setTitle("Not connected");
-                
-        	}
-        }
-    }
-    
-    
-    private void setTitle(final String title) {
-        runOnUiThread(new Runnable() {
-            public void run() {
-            	IJacketScanActiv.super.setTitle("OSNAP - " + title);
-            	}
-            });
-    }
 
     private void quickToastMessage(final String message) {
         this.runOnUiThread(new Runnable() {
@@ -329,7 +226,6 @@ public class IJacketScanActiv extends Activity{// implements OnItemSelectedListe
     @Override
     public void onDestroy() {
     	super.onDestroy();
-    	disconnect();
     }
     
     @Override
@@ -341,22 +237,15 @@ public class IJacketScanActiv extends Activity{// implements OnItemSelectedListe
             	Log.d("LOG_TAG", "scan OK" );
                 String macAddress = intent.getStringExtra("SCAN_RESULT");
                 
+                SharedPreferences mypref = getSharedPreferences(IJacketApp.PREF_FILE_NAME, MODE_PRIVATE);
+                SharedPreferences.Editor e = mypref.edit();
+				e.putString(IJacketApp.MAC_PREFERENCE_TAG, macAddress);
+				e.commit();
+                
                 // Handle successful scan
-                try {
-                	BluetoothConnection con = new BluetoothConnection(macAddress, this, getConnectionListener(this));
-                	Log.d("LOG_TAG", "trying to connect" );
-                	IJacketApp appState = ((IJacketApp)getApplicationContext());
-                	appState.clearRetryCounter();
-                	con.connect();
-					
-					appState.setCon(con);
-					appState.setMacAddress(macAddress);
-					
-				} catch (Exception e) {
-					Log.d("LOG_TAG", "exception trying to connect" );
-					quickToastMessage(e.getMessage());
-	            	resetUI();
-				}
+				Intent i = new Intent(IJacketScanActiv.this, JacketMenuActivity.class);
+				startActivity(i);
+
             } 
             
             //Failed scan
@@ -370,38 +259,7 @@ public class IJacketScanActiv extends Activity{// implements OnItemSelectedListe
 
 
 
-/*	@Override
-	public void onItemSelected(AdapterView<?> parent, View view, int pos,
-			long id) {
-		
-		Cursor cursor = (Cursor) parent.getItemAtPosition(pos);
-		int i = cursor.getColumnIndex(SocialContract.Communities.GLOBAL_ID); 
-		String comJid = cursor.getString(i);
-		i = cursor.getColumnIndex(SocialContract.Communities._ID); 
-		String comLocalId = cursor.getString(i);
-	    Log.d("LOG_TAG", "found community with JID " + comJid + " and id " + comLocalId);
-	    
-		IJacketApp appState = ((IJacketApp)getApplicationContext());
-		appState.setSelectCommunityJid(comJid);
-		appState.setSelectedCommunityLocalId(comLocalId);
 
-		
-	}
-
-	
-
-
-	@Override
-	public void onNothingSelected(AdapterView<?> arg0) {
-		// TODO Auto-generated method stub
-		
-	}*/
-
-	
-	// LOADER CLASS
-//	public static class MyActivCursorLoader extends ListFragment implements LoaderCallbacks<Cursor> {
-
-	//}
 	
 }
 
