98612a88e5d22154a9c8755dd58ffd1b915a4650
Patrick Skillen <p.skillen@hw.ac.uk>
2013-06-28 18:18:32 +0100

    Trying a new XMPP package

--- 3rdPartyServices/StudentServices/CollaborativeQuiz/SocialLearningGame/CommsFrwk/CommsManager.cs
+++ 3rdPartyServices/StudentServices/CollaborativeQuiz/SocialLearningGame/CommsFrwk/CommsManager.cs
@@ -1,31 +1,51 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using agsXMPP;
-using agsXMPP.protocol.extensions.pubsub;
+using jabber;
+using jabber.connection;
 
 namespace CommsFrwk
 {
     public class CommsManager
     {
-        private readonly XmppClientConnection conn;
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(CommsManager));
+
         private readonly PubSubManager pubSubManager;
+
+        private readonly String hostUrl;
+        private readonly String username;
 
         public CommsManager(String hostUrl, String username, String password)
         {
-            conn = new XmppClientConnection();
-            conn.Server = hostUrl;
-            conn.Username = username;
-            conn.Password = password;
+            this.hostUrl = hostUrl;
+            this.username = username;
 
-            pubSubManager = new PubSubManager(conn);
+            if (log.IsDebugEnabled)
+                log.Debug("Logging in to " + hostUrl);
+
+            pubSubManager = new PubSubManager();
         }
 
 
         //       Node ensureNodeExists(String nodeName) throws XMPPException;
 
         //<T> void registerListener(String nodeName, String myJID, Class<T> expectedClass, IncomingNodeMessageListener<T> eventHandler) throws XMPPException;
+        public void RegisterListener(String nodeName)
+        {
+            log.Debug("Registering listener for " + nodeName);
+
+            JID jid = new JID(username + "@" + hostUrl);
+
+            PubSubNode node = pubSubManager.GetNode(jid, nodeName, 1);
+            node.OnItemPublished += node_OnItemPublished;
+            node.Subscribe();
+        }
+
+        void node_OnItemPublished(PubSubNode node, jabber.protocol.iq.PubSubItem item)
+        {
+            log.Debug(String.Format("node_OnItemPublished {0} \n {1} \n {2} ",
+                node,
+                item));
+        }
+
 
         //<T> void unregisterListener(String nodeName, IncomingNodeMessageListener<T> eventHandler) throws XMPPException;
 
