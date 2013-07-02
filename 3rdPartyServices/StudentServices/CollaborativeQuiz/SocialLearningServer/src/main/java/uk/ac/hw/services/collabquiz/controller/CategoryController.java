400e6c240c36a48182f5a4e5774c54b7aa188a46
cf83 <cf83@hw.ac.uk>
2013-07-02 12:19:27 +0100

    Interim commit to send to Paddy

---
+++ 3rdPartyServices/StudentServices/CollaborativeQuiz/SocialLearningServer/src/main/java/uk/ac/hw/services/collabquiz/controller/CategoryController.java
@@ -0,0 +1,38 @@
+package uk.ac.hw.services.collabquiz.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import uk.ac.hw.services.collabquiz.dao.ICategoryRepository;
+import uk.ac.hw.services.collabquiz.dao.CategoryRepository;
+import uk.ac.hw.services.collabquiz.entities.Category;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import java.util.List;
+
+@Controller
+@ManagedBean(name = "categoryController")
+public class CategoryController {
+    private static final Logger log = LoggerFactory.getLogger(CategoryController.class);
+
+    private ICategoryRepository categoryRepository;
+
+    private List<Category> categories;
+
+    public CategoryController() {
+        log.debug("CategoryController ctor()");
+        categoryRepository = new CategoryRepository();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        log.debug("postConstruct()");
+
+        categories = categoryRepository.list();
+    }
+
+    public List<Category> getCategories() {
+        return categories;
+    }
+}
---
+++ 3rdPartyServices/StudentServices/CollaborativeQuiz/SocialLearningServer/src/main/java/uk/ac/hw/services/collabquiz/controller/QuestionController.java
@@ -0,0 +1,48 @@
+package uk.ac.hw.services.collabquiz.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import uk.ac.hw.services.collabquiz.dao.IQuestionRepository;
+import uk.ac.hw.services.collabquiz.dao.QuestionRepository;
+import uk.ac.hw.services.collabquiz.entities.Question;
+
+import javax.annotation.PostConstruct;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
+import java.util.List;
+
+@Controller
+@ManagedBean(name = "questionController")
+@ViewScoped
+public class QuestionController {
+    private static final Logger log = LoggerFactory.getLogger(QuestionController.class);
+
+    private IQuestionRepository questionRepository;
+
+    private List<Question> questions;
+
+    public QuestionController() {
+        log.debug("QuestionController ctor()");
+        questionRepository = new QuestionRepository();
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+        log.debug("postConstruct()");
+
+        questions = questionRepository.list();
+    }
+
+    public List<Question> getQuestions() {
+        return questions;
+    }
+
+    public void growl() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        context.addMessage(null, new FacesMessage("Second Message", "Additional Info Here..."));
+    }
+
+}
