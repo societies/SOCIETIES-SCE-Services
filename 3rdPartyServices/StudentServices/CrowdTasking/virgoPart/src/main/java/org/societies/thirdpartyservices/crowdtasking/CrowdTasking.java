dd428d156f1a7d51177629fe0818e02221ad7cc6
Simon <setcce.research@gmail.com>
2013-10-15 11:38:58 +0200

    Crowd Tasking Virgo part

---
+++ 3rdPartyServices/StudentServices/CrowdTasking/virgoPart/src/main/java/org/societies/thirdpartyservices/crowdtasking/CrowdTasking.java
@@ -0,0 +1,199 @@
+/**
+ * Copyright (c) 2011, SOCIETIES Consortium (WATERFORD INSTITUTE OF TECHNOLOGY (TSSG), HERIOT-WATT UNIVERSITY (HWU), SOLUTA.NET 
+ * (SN), GERMAN AEROSPACE CENTRE (Deutsches Zentrum fuer Luft- und Raumfahrt e.V.) (DLR), Zavod za varnostne tehnologije
+ * informacijske družbe in elektronsko poslovanje (SETCCE), INSTITUTE OF COMMUNICATION AND COMPUTER SYSTEMS (ICCS), LAKE
+ * COMMUNICATIONS (LAKE), INTEL PERFORMANCE LEARNING SOLUTIONS LTD (INTEL), PORTUGAL TELECOM INOVAÇÃO, SA (PTIN), IBM Corp., 
+ * INSTITUT TELECOM (ITSUD), AMITEC DIACHYTI EFYIA PLIROFORIKI KAI EPIKINONIES ETERIA PERIORISMENIS EFTHINIS (AMITEC), TELECOM 
+ * ITALIA S.p.a.(TI),  TRIALOG (TRIALOG), Stiftelsen SINTEF (SINTEF), NEC EUROPE LTD (NEC))
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.societies.thirdpartyservices.crowdtasking;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.societies.api.comm.xmpp.interfaces.ICommManager;
+import org.societies.api.context.broker.ICtxBroker;
+import org.societies.api.identity.IIdentity;
+import org.societies.api.identity.IIdentityManager;
+import org.societies.api.osgi.event.CSSEvent;
+import org.societies.api.osgi.event.EventListener;
+import org.societies.api.osgi.event.IEventMgr;
+import org.societies.api.osgi.event.InternalEvent;
+import org.societies.api.schema.servicelifecycle.model.ServiceResourceIdentifier;
+import org.societies.api.services.IServices;
+import org.societies.api.services.ServiceMgmtEvent;
+import org.societies.api.services.ServiceMgmtEventType;
+import org.societies.thirdpartyservices.crowdtasking.tools.SocketServer;
+
+
+/**
+ * Describe your class here...
+ *
+ * @author Simon Jureša
+ *
+ */
+public class CrowdTasking extends EventListener implements ICrowdTasking{
+
+	Logger logging = LoggerFactory.getLogger(this.getClass());
+	private ServiceResourceIdentifier myServiceID;
+	private IIdentity serverIdentity;
+	//private Requestor requestor;
+	private IIdentityManager idMgr;
+	private IServices serviceMgmt;
+	private IEventMgr eventMgr;
+	private ICtxBroker ctxBroker;
+	private ICommManager commManager;
+
+	public void init(){
+		this.logging.info("init CrowdTaskingVirgoPart:");
+		SocketServer server = new SocketServer(this);
+		new Thread(server).start();
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.societies.api.osgi.event.EventListener#handleInternalEvent(org.societies.api.osgi.event.InternalEvent)
+	 */
+	@Override
+	public void handleInternalEvent(InternalEvent event) {
+		// This method is called after the bundle has been successfully installed on virgo
+		//and we need this to receive the AskFree service identifier (ServiceResourceIdentifier)
+
+		logging.info("Received internal event: "+event.geteventName());
+
+		if(event.geteventName().equalsIgnoreCase("NEW_SERVICE")){
+			logging.info("Received SLM event");
+			ServiceMgmtEvent slmEvent = (ServiceMgmtEvent) event.geteventInfo();
+			this.logging.info("SLM event Bundle Symbol Name" + slmEvent.getBundleSymbolName());
+			if (slmEvent.getBundleSymbolName().equalsIgnoreCase("org.societies.thirdpartyservices.crowdtasking.CrowdTasking")){
+				this.logging.info("Received SLM event for my bundle");
+				if (slmEvent.getEventType().equals(ServiceMgmtEventType.NEW_SERVICE)){
+
+					setMyServiceID(slmEvent.getServiceId());
+					this.logging.info("1.Service id:" + slmEvent.getServiceId().toString());
+					//GET ID OF SERVER
+					this.setServerIdentity(this.idMgr.getThisNetworkNode());
+					//this.setServerIdentity(serviceMgmt.getServer(slmEvent.getServiceId()));
+					logging.info("2.Servers Identity: " + getServerIdentity());
+
+					//SocketServer server = new SocketServer(getMyServiceID(), getServerIdentity());
+					SocketServer server = new SocketServer(this);
+					new Thread(server).start();
+					/////////////////////////////////////////////////////////////////////////////
+					//this.requestor = new RequestorService(serverIdentity, myServiceID);
+				}
+			}
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see org.societies.api.osgi.event.EventListener#handleExternalEvent(org.societies.api.osgi.event.CSSEvent)
+	 */
+	@Override
+	public void handleExternalEvent(CSSEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param myServiceID the myServiceID to set
+	 */
+	public void setMyServiceID(ServiceResourceIdentifier myServiceID) {
+		this.myServiceID = myServiceID;
+		this.logging.debug("MyServiceId is set");
+	}
+
+	
+	public ServiceResourceIdentifier getMyServiceID() {
+		return myServiceID;
+	}
+
+	/**
+	 * @return the serverIdentity
+	 */
+	public IIdentity getServerIdentity() {
+		this.logging.debug("Get ServerId:" +serverIdentity.toString());
+		return serverIdentity;
+	}
+
+	/**
+	 * @param serverIdentity the serverIdentity to set
+	 */
+	public void setServerIdentity(IIdentity serverIdentity) {
+		this.logging.debug("ServerId is set");
+		this.serverIdentity = serverIdentity;
+	}
+
+	/**
+	 * @return the serviceMgmt
+	 */
+	public IServices getServiceMgmt() {
+		return serviceMgmt;
+	}
+
+	/**
+	 * @param serviceMgmt the serviceMgmt to set
+	 */
+	public void setServiceMgmt(IServices serviceMgmt) {
+		this.serviceMgmt = serviceMgmt;
+	}
+
+	/**
+	 * @return the eventMgr
+	 */
+	public IEventMgr getEventMgr() {
+		return eventMgr;
+	}
+
+	/**
+	 * @param eventMgr the eventMgr to set
+	 */
+	public void setEventMgr(IEventMgr eventMgr) {
+		this.eventMgr = eventMgr;
+	}
+
+	/**
+	 * @return the commManager
+	 */
+	public ICommManager getCommManager() {
+		return commManager;
+	}
+
+	/**
+	 * @param commManager the commManager to set
+	 */
+	public void setCommManager(ICommManager commManager) {
+		this.commManager = commManager;
+		this.idMgr = this.commManager.getIdManager();
+	}
+
+	/**
+	 * @return the ctxBroker
+	 */
+	public ICtxBroker getCtxBroker() {
+		this.logging.debug("get ContextBroker");
+		return ctxBroker;
+	}
+
+	/**
+	 * @param ctxBroker the ctxBroker to set
+	 */
+	public void setCtxBroker(ICtxBroker ctxBroker) {
+		this.ctxBroker = ctxBroker;
+	}
+
+}
---
+++ 3rdPartyServices/StudentServices/CrowdTasking/virgoPart/src/main/java/org/societies/thirdpartyservices/crowdtasking/tools/ClientHandler.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright (c) 2011, SOCIETIES Consortium (WATERFORD INSTITUTE OF TECHNOLOGY (TSSG), HERIOT-WATT UNIVERSITY (HWU), SOLUTA.NET 
+ * (SN), GERMAN AEROSPACE CENTRE (Deutsches Zentrum fuer Luft- und Raumfahrt e.V.) (DLR), Zavod za varnostne tehnologije
+ * informacijske družbe in elektronsko poslovanje (SETCCE), INSTITUTE OF COMMUNICATION AND COMPUTER SYSTEMS (ICCS), LAKE
+ * COMMUNICATIONS (LAKE), INTEL PERFORMANCE LEARNING SOLUTIONS LTD (INTEL), PORTUGAL TELECOM INOVAÇÃO, SA (PTIN), IBM Corp., 
+ * INSTITUT TELECOM (ITSUD), AMITEC DIACHYTI EFYIA PLIROFORIKI KAI EPIKINONIES ETERIA PERIORISMENIS EFTHINIS (AMITEC), TELECOM 
+ * ITALIA S.p.a.(TI),  TRIALOG (TRIALOG), Stiftelsen SINTEF (SINTEF), NEC EUROPE LTD (NEC))
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+ * conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+ *    disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING,
+ * BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT 
+ * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.societies.thirdpartyservices.crowdtasking.tools;
+
+import java.io.IOException;
+import java.io.InvalidClassException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.societies.thirdpartyservices.crowdtasking.CrowdTasking;
+
+
+/**
+ * Describe your class here...
+ *
+ * @author Simon Jureša
+ *
+ */
+public class ClientHandler implements Runnable {
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	private Socket socket;
+	private ObjectOutputStream out ;
+	private ObjectInputStream in = null;
+
+	private String cssId;
+	private CrowdTasking crowdTasking;
+
+	public ClientHandler(Socket socket, CrowdTasking crowdTasking){
+		this.socket = socket;
+		this.crowdTasking = crowdTasking;
+		this.log.debug("Client Handler created");
+	}
+
+	public void run(){
+		this.log.debug("In run method");
+
+		this.getStreams();
+		this.getClientMessage();
+		//this.sendMessage(message);
+	}
+
+	private void getStreams(){
+		try{
+			out = new ObjectOutputStream(socket.getOutputStream());
+			out.flush();
+			this.log.debug("OutputStream created");
+
+			in = new ObjectInputStream(socket.getInputStream());
+			this.log.debug("Input Stream created");
+		}catch(IOException e){
+			this.log.debug("ERROR CREATING INPUT/OUTPUT STREAM:" + e.getMessage());
+			this.closeConnection();
+		}
+	}
+
+	private void getClientMessage(){
+		try{
+			Object x;
+			boolean reading = true;
+			while(reading){
+				x=in.readObject();
+				if(x instanceof String){
+					cssId = (String)x;			
+					this.log.debug("received cssId: " + cssId + " from Android");
+					if (crowdTasking.getMyServiceID() != null){
+						this.sendMessage(crowdTasking.getMyServiceID().toString());
+					}
+					else {
+						this.sendMessage("I am a message from the Virgo.");
+					}
+				}
+			}		
+		}catch(ClassNotFoundException e){
+			this.log.debug("ClassNotFoundException: " + e.getMessage());
+		}catch(InvalidClassException e){
+			this.log.debug("InvalidClassException: " + e.getMessage());
+		}catch(IOException e) {
+			this.log.debug("IOException: " + e.getMessage());
+		}
+	}
+
+	public void sendMessage(String message){
+		this.log.debug("In sendMessage");
+		try{
+			this.log.debug("Attempting to send location: " + message);
+			out.writeObject(message);
+			this.log.debug("Message sent to client: " + message);
+			out.flush();
+			out.reset();
+		}
+		catch(IOException ioException){
+			this.log.debug("ERROR WHILE SENDING LOCATION!" + ioException.getStackTrace());
+		}
+	}
+
+	private void closeConnection(){
+		try{
+			this.log.debug("Closing connection");
+			in.close();
+			out.close();
+			socket.close();
+		}catch(IOException e){
+			this.log.debug("ERROR CLOSING CONNECTION" + e.getMessage());
+		}
+	}
+}
